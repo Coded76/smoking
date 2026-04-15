d90dee1853595ab3054b4b9ca33083f28b001cac
Coded76 <0xhope66@gmail.com>
2026-04-16 00:16:11 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Launch Page",
   description: "Contract address showcase",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>💨</text></svg>",
+  },
 };
 
 export default function RootLayout({
